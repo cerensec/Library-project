b1dbcc9f374ed98568dd2535f0fbb7c7fd2df3cf
Nathan VIAUD <vnat38@gmail.com>
2022-12-11 17:07:28 +0100

    Auto stash before rebase of "origin/main"

--- front/src/shared/stores/bookStore.ts
+++ front/src/shared/stores/bookStore.ts
@@ -1,52 +1,69 @@
-import { defineStore } from "pinia";
-import { Book, BookForm } from "../interfaces";
-import { getBooks, deleteBook, editBook, createBook, getLangs, getBook } from "../services";
+import { defineStore } from 'pinia';
+import { Book, BookForm, Format, Language } from '../interfaces';
+import {
+    getBooks,
+    deleteBook,
+    editBook,
+    createBook,
+    getLangs,
+    getBook,
+    getFormats,
+} from '../services';
 
 interface BookState {
     books: Book[];
     search: string;
     isLoading: boolean;
     needRefresh: boolean;
-    langs: string[];
+    langs: Language[];
+    formats: Format[];
 }
 
 export const useBooks = defineStore('books', {
-    state: ():BookState  => ({
+    state: (): BookState => ({
         books: [],
         search: '',
         isLoading: true,
         needRefresh: false,
-        langs: []
+        langs: [],
+        formats: [],
     }),
     getters: {
         filteredBooks(state): Book[] {
-            return state.books.filter(book => book.title.toLowerCase().includes(state.search.toLowerCase()));
-        }
+            return state.books.filter((book) =>
+                book.title.toLowerCase().includes(state.search.toLowerCase())
+            );
+        },
     },
     actions: {
         async fetchBooks() {
             this.isLoading = true;
             this.books = await getBooks();
             this.langs = await getLangs();
+            this.formats = await getFormats();
             this.isLoading = false;
             this.needRefresh = false;
         },
 
-        async deleteBook(bookId: number) {
-            const bookIndex = this.books.findIndex(book => book.id === bookId);
-            if(bookIndex !== -1) {
-               await deleteBook(bookId);
-               this.books.splice(bookIndex, 1);
+        async deleteBook(bookId: string) {
+            const bookIndex = this.books.findIndex(
+                (book) => book.isbn === bookId
+            );
+            if (bookIndex !== -1) {
+                await deleteBook(bookId);
+                this.books.splice(bookIndex, 1);
             }
             this.needRefresh = true;
         },
 
-        async editBook(bookId: number, bookForm: BookForm) {
+        async editBook(bookId: string, bookForm: BookForm) {
             const editedBook = await editBook(bookId, bookForm);
-            if(editedBook) {
+            if (editedBook) {
                 this.needRefresh = true;
-                const bookIndex = this.books.findIndex(book => book.id === bookId);
-                if(bookIndex !== -1) {
+                const bookIndex = this.books.findIndex(
+                    (book) => book.isbn === bookId
+                );
+                if (bookIndex !== -1) {
                     this.books[bookIndex] = editedBook;
                 }
             }
@@ -54,14 +71,26 @@ export const useBooks = defineStore('books', {
 
         async addBook(book: Book) {
             const newBook = await createBook(book);
-            if(newBook) {
+            if (newBook) {
                 // this.books.push(newBook);
                 this.needRefresh = true;
             }
         },
 
         getBook(bookId: string): Book {
-            return this.books[this.books.findIndex(book => book.isbn === bookId)];
-        }
-    }
-})
+            return this.books[
+                this.books.findIndex((book) => book.isbn === bookId)
+            ];
+        },
+
+        getLang(lang: string): Language {
+            return this.langs[this.langs.findIndex((l) => l.name === lang)];
+        },
+
+        getFormat(format: string): Format {
+            return this.formats[
+                this.formats.findIndex((f) => f.name === format)
+            ];
+        },
+    },
+});
