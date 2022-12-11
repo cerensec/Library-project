e15134b6e5afa706f1bc6882e06e25e8791d5cde
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2022-12-11 10:08:05 +0100

    Linked front: WIP

--- front/src/components/BookItem.vue
+++ front/src/components/BookItem.vue
@@ -7,12 +7,13 @@ defineProps<{
 </script>
 
 <template>
-    <router-link :to="{ path: '/detail/' + book.id }">
+    <router-link :to="{ path: '/detail/' + book.isbn }">
         <div class="card text-center">
             <img class="w-56 mb-3 m-auto" src="https://m.media-amazon.com/images/I/81A480IJsjL._AC_UL640_QL65_.jpg" alt="">
             <h1 class="text-2xl">{{book.title}}</h1>
-            <p>{{book.resume}}</p>
-            <small>{{book.author}}</small>
+            <p>{{book.overview}}</p>
+            <!-- <small>{{book.author}}</small> -->
         </div>
     </router-link>
 </template>
+
