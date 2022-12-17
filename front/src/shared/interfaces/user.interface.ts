5ab7365b450db57d71839e553b3860483544271c
Nathan VIAUD <vnat38@gmail.com>
2022-12-17 12:21:51 +0100

    fonctional adding a book

---
+++ front/src/shared/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+    number: number;
+    username: string;
+    password: string;
+    role: string;
+    opened: boolean;
+    state: string;
+}
--- front/src/shared/services/book.service.ts
+++ front/src/shared/services/book.service.ts
@@ -2,41 +2,26 @@ import type { Book, BookForm, Format, Language } from '../interfaces';
 
 const API_URL = '/api/catalog';
 
-// const book: Book  = { title: "Titre", resume: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repudiandae cumque provident, culpa veniam sit magnam nulla laboriosam perferendis nam, dolore debitis deleniti fuga. Sint corrupti distinctio libero cumque temporibus?", author: "Auteur", id: 1, language: "Français", numberOfPages: 100, format: "Broché" };
-
-// let books: Book[] = [];
-// for(let i = 1; i < 11; i++) {
-//     const myBook = {title: book.title + i, resume: book.resume, author: book.author, id: i, language: book.language, numberOfPages: Math.random() * (999 - 1) + 1, format: book.format};
-//     books.push(myBook);
-// }
-
 export async function getBooks(): Promise<Book[]> {
-    // return books;
     return await (await fetch(`${API_URL}/getAll`)).json();
 }
 
 export async function getBook(bookId: string): Promise<Book> {
     const book = await (await fetch(`${API_URL}/getById/${bookId}`)).json();
     return book;
-
-    // const res = books.find(book => book.id === bookId);
-    // if (res) {
-    //     return res;
-    // } else {
-    //     throw new Error('Book not found');
-    // }
 }
 
-export async function createBook(book: Book) {
+export async function createBook(book: Book): Promise<Book> {
+    console.log(book.isbn);
     try {
-        const response = await fetch(`${API_URL}/savebook`, {
+        const response = await fetch(`${API_URL}/saveBook`, {
             method: 'POST',
             body: JSON.stringify(book),
             headers: {
                 'Content-type': 'application/json',
             },
         });
-        return response;
+        return response.json();
     } catch (error) {
         throw error;
     }
@@ -53,13 +38,6 @@ export async function editBook(bookId: string, book: BookForm): Promise<Book> {
         })
     ).json();
     return updatedBook;
-    // const bookIndex = books.findIndex(book => book.id === bookId);
-    // if(bookIndex !== -1) {
-    //     books[bookIndex] = { ...books[bookIndex], ...book };
-    //     return books[bookIndex];
-    // } else {
-    //     throw new Error('Book not found');
-    // }
 }
 
 export async function deleteBook(bookId: string): Promise<string> {
@@ -67,19 +45,4 @@ export async function deleteBook(bookId: string): Promise<string> {
         method: 'DELETE',
     });
     return bookId;
-    // const index = books.findIndex(book => book.id === bookId);
-    // if (index !== -1) {
-    //     books.splice(index, 1);
-    //     return bookId;
-    // } else {
-    //     throw new Error('Book not found');
-    // }
-}
-
-export async function getLangs(): Promise<Language[]> {
-    return await (await fetch(`api/language/getAll`)).json();
-}
-
-export async function getFormats(): Promise<Format[]> {
-    return await (await fetch('api/format/getAll')).json();
 }
